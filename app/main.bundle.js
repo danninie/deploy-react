3d6fee482fe431cb531ee162737097d16eaa7496
danninie <xminzhang@aliyun.com>
2021-08-05 16:48:35 +0800

    Updates

--- app/main.bundle.js
+++ app/main.bundle.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cc94b18861a636f6ab5c";
+/******/ 	var hotCurrentHash = "20c9310773ee4e99d3b9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -6149,7 +6149,7 @@ const initialState = {
 const setLocale = locale => (dispatch) => Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__awaiter"])(void 0, void 0, void 0, function* () {
     if (!Object.keys(react_jhipster__WEBPACK_IMPORTED_MODULE_2__["TranslatorContext"].context.translations).includes(locale)) {
         //const response = await axios.get(`i18n/${locale}.json?buildTimestamp=${process.env.BUILD_TIMESTAMP}`, { baseURL: '' });
-        const response = yield axios__WEBPACK_IMPORTED_MODULE_1___default.a.get(`/deploy-react/i18n/${locale}.json?buildTimestamp=${'1628143643909'}`, { baseURL: '' });
+        const response = yield axios__WEBPACK_IMPORTED_MODULE_1___default.a.get(`/deploy-react/i18n/${locale}.json?buildTimestamp=${'1628152067603'}`, { baseURL: '' });
         react_jhipster__WEBPACK_IMPORTED_MODULE_2__["TranslatorContext"].registerTranslations(locale, response.data);
     }
     dispatch({
